6d5b459a8f2e3166f5a6e66a88ae1003dbaff8e4
root <matheuslds1@gmail.com>
2018-09-29 08:42:34 +0000

    ...

---
+++ config/index.js
@@ -0,0 +1,31 @@
+
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsPublicPath: './',
+    assetsSubDirectory: 'static',
+    productionSourceMap: false,
+    devtool: '#source-map',
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    env: require('./dev.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    port: process.env.PORT || 8080,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
+    productionSourceMap: false,
+    devtool: '#source-map',
+    proxyTable: {},
+    cssSourceMap: false
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import App from './App';
+import router from '@/router/index';
+import Vuetify from 'vuetify';
+import axios from 'axios';
+import VueCookies from 'vue-cookies';
+
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify,{
+  iconfont: 'fa'
+});
+Vue.use(VueCookies);
+Vue.http = Vue.prototype.$http = axios;
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+});
